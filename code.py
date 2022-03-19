14b1bf1a3d9ff0b0d375460f73049181f8d30d51
brend3n <brenden@knights.ucf.edu>
2022-03-19 11:59:23 -0400

    Added whitelist and code.py

---
+++ code.py
@@ -0,0 +1,34 @@
+import requests
+import os
+import sys
+
+g_whitelist = []
+
+
+#! TODO: Not tested
+# Loads the whitelist values into the script
+def load_whitelist():
+    with open("whitelist.txt", "r") as f:
+       g_whitelist = f.readlines()
+
+#! TODO: Not tested
+# Updates the whitelist with a new entry
+def update_whitelist(entry: str):
+    g_whitelist.append(entry)
+
+#! TODO: Not tested
+# Save the current whitelist locally to whitelist.txt
+def save_whitelist():
+    with open("whitelist.txt", "") as f:
+        pass
+
+def main():
+    load_whitelist()
+    
+
+
+
+    save_whitelist()
+
+if __name__ == "__main__":
+    main()
